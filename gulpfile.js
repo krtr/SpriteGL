24dc94050065dc31cc3eb8bcb963e8371b23d77a
Krzysztof Tryniecki <krzszt@outlook.com>
2014-12-12 21:14:40 +0100

    Fixed build

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,15 @@ var gulp = require("gulp");
 var typescript = require("gulp-typescript");
 var concat = require("gulp-concat");
 
-gulp.task("build-src", function () {
-	gulp.src("./SpriteGL/*.ts")
-	.pipe(typescript({ sortOutput: true, target: "ES5", removeComments: true }))
-	.pipe(concat("SpriteGL.js"))
-	.pipe(gulp.dest("./bin"));
-});
+gulp.task("build", function () {
+	var tsResult = gulp.src("./SpriteGL/*.ts")
+					.pipe(typescript({ sortOutput: true, target: "ES5", removeComments: true, declarationFiles: true }));
 
-gulp.task("build-decl", function () {
-	gulp.src("./SpriteGL/*.ts")
-	.pipe(typescript({ declarationFiles: true }))
-	.pipe(gulp.dest("./bin"));
-});
+	tsResult.dts
+		.pipe(concat("SpriteGL.d.ts"))
+		.pipe(gulp.dest("./bin"));
 
+	tsResult.js
+		.pipe(concat("SpriteGL.js"))
+		.pipe(gulp.dest("./bin"));
+});
